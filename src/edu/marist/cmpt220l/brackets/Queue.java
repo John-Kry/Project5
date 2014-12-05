1e4759f8700c048cf1b3e1001c287e0c1100617b
jibbey <john.kryspin1@marist.edu>
2014-12-04 20:28:17 -0500

    Queue functional?

--- src/edu/marist/cmpt220l/brackets/Queue.java
+++ src/edu/marist/cmpt220l/brackets/Queue.java
@@ -1,24 +1,44 @@
 package edu.marist.cmpt220l.brackets;
 
+import edu.marist.cmpt220l.olympians.Olympian;
+import edu.marist.cmpt220l.olympians.Sex;
+import edu.marist.cmpt220l.teams.Team;
+
 /**
  * Created by john on 12/3/2014.
  */
 public class Queue {
+    public static void main(String[] args){
+        Queue newQueue = new Queue();
+        newQueue.enqueue(new Team(new Olympian("John", Sex.MALE,17),new Olympian("Jacob", Sex.MALE,17)));
+        newQueue.enqueue(new Team(new Olympian("Geoff", Sex.MALE,17),new Olympian("George", Sex.MALE,17)));
+        System.out.println(newQueue.first.value);
+        System.out.println(newQueue.last.value);
+        newQueue.dequeue();
+        System.out.println(newQueue.first.value);
+        System.out.println(newQueue.last.value);
+        newQueue.dequeue();
+        newQueue.dequeue();
+    }
     private QueueItem first;
     private QueueItem last;
-    public void enqueue(int value){
-        QueueItem newVal = new QueueItem(value);
+    public void enqueue(Team team){
+        QueueItem newTeam = new QueueItem(team);
         if (last!=null){
-            last.next = newVal;
-            last = newVal;
+            last.next = newTeam;
+            last = newTeam;
+        }else{
+            first = newTeam;
+            last = newTeam;
         }
     }
-    public int dequeue(){
-        if (first ==null){
+    public Team dequeue(){
+        if (first == null){
             System.out.println("Error removing teams");
+            return null;
         }
-        QueueItem currVal = first;
+        QueueItem currTeam = first;
         first = first.next;
-        return currVal.value;
+        return currTeam.value;
     }
 }
--- src/edu/marist/cmpt220l/brackets/QueueItem.java
+++ src/edu/marist/cmpt220l/brackets/QueueItem.java
@@ -1,13 +1,15 @@
 package edu.marist.cmpt220l.brackets;
 
+import edu.marist.cmpt220l.teams.Team;
+
 /**
  * Created by john on 12/3/2014.
  */
 public class QueueItem {
     public QueueItem next;
-    public int value;
+    public Team value;
 
-    public QueueItem(int value) {
-        this.value = value;
+    public QueueItem(Team team) {
+        this.value = team;
     }
 }
