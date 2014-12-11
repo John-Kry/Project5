e677edee02a6a65d5541ad389445be68547c694d
jibbey <john.kryspin1@marist.edu>
2014-12-10 22:01:31 -0500

    fixed stack(again)

--- src/edu/marist/cmpt220l/brackets/Stack.java
+++ src/edu/marist/cmpt220l/brackets/Stack.java
@@ -12,13 +12,26 @@ public class Stack {
 
     public static void main(String[] args){
         Stack newStack = new Stack();
-
         newStack.push(new Team(new Olympian("chad", Sex.MALE, 17), new Olympian("George", Sex.MALE, 17)));
         newStack.push(new Team(new Olympian("jeff", Sex.MALE, 17), new Olympian("penolope", Sex.MALE, 17)));
+        newStack.push(new Team(new Olympian("Gross", Sex.MALE, 17), new Olympian("Annoy", Sex.MALE, 17)));
+        newStack.push(new Team(new Olympian("Gross", Sex.MALE, 17), new Olympian("Annoy", Sex.MALE, 17)));
+        newStack.push(new Team(new Olympian("Gross", Sex.MALE, 17), new Olympian("Annoy", Sex.MALE, 17)));
+        newStack.push(new Team(new Olympian("Gross", Sex.MALE, 17), new Olympian("Annoy", Sex.MALE, 17)));
+        newStack.push(new Team(new Olympian("HELLOI", Sex.MALE, 17), new Olympian("HEY", Sex.MALE, 17)));
+        newStack.push(new Team(new Olympian("Gross", Sex.MALE, 17), new Olympian("Annoy", Sex.MALE, 17)));
         System.out.println(newStack.top.value);
         newStack.pop();
         System.out.println(newStack.top.value);
-        newStack.push(new Team(new Olympian("Gross", Sex.MALE, 17), new Olympian("Annoy", Sex.MALE, 17)));
+        newStack.pop();
+        System.out.println(newStack.top.value);
+        newStack.pop();
+        System.out.println(newStack.top.value);
+        newStack.pop();
+        System.out.println(newStack.top.value);
+        newStack.pop();
+        System.out.println(newStack.top.value);
+        newStack.pop();
         System.out.println(newStack.top.value);
         newStack.pop();
         System.out.println(newStack.top.value);
@@ -35,7 +48,6 @@ public class Stack {
     public void push(Team team){
         StackItem newTeam = new StackItem(team);
         if (top != null){
-            top.down = newTeam;
             newTeam.down = top;
             top = newTeam;
         }else{
